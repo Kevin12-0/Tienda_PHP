8832a41751630816fca1ef72b3b2252f8aa3b654
Kecvin-12-u <65631970+Kevin12-0@users.noreply.github.com>
2023-03-16 23:11:09 -0600

    hols mundo

--- Productos/delete_datos.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $id_producto = $_GET["id_producto"];
-    $datos = new SQLite3("tienda.db");
-    $datos->exec("DELETE FROM PRODUCTOS where id_producto='$id_producto';");
-    header("Location: index.php");
-?>
